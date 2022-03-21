7fd200791367bca68c49a95171c19c3075d239a2
MichaelScarantino <scarantino.michael@outlook.it>
2022-03-21 12:45:28 +0100

    Done migration-seeder-model

---
+++ config/dishes.php
@@ -0,0 +1,138 @@
+<?php
+return [
+    [
+        'name' => 'Capricciosa',
+        'description' => 'Pizza Capricciosa',
+        'path_img' => 'https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 7.50,
+        'visible' => true,
+        'restaurant_id' => 1,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Lasagne',
+        'description' => 'Lasagne alla bolognese',
+        'path_img' => 'https://images.pexels.com/photos/4162496/pexels-photo-4162496.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 9.99,
+        'visible' => true,
+        'restaurant_id' => 2,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Carbonara',
+        'description' => 'Pasta alla carbonara',
+        'path_img' => 'https://images.pexels.com/photos/1435898/pexels-photo-1435898.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 8.99,
+        'visible' => true,
+        'restaurant_id' => 1,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Tonkatsu',
+        'description' => 'Cotoletta impanata',
+        'path_img' => 'https://images.pexels.com/photos/37352/food-japanese-asian.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 4.99,
+        'visible' => true,
+        'restaurant_id' => 2,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Ramen',
+        'description' => 'Zuppa di carne',
+        'path_img' => 'https://images.pexels.com/photos/1907229/pexels-photo-1907229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 6.99,
+        'visible' => false,
+        'restaurant_id' => 2,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Okonomiyaki',
+        'description' => 'Frittata di verdure',
+        'path_img' => 'https://www.viaggioff.it/wp-content/uploads/2021/12/Okonomiyaki-scaled.jpg',
+        'price' => 7.99,
+        'visible' => false,
+        'restaurant_id' => 2,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Tacos',
+        'description' => 'Tortilla messicane',
+        'path_img' => 'https://images.pexels.com/photos/4958792/pexels-photo-4958792.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 8.99,
+        'visible' => true,
+        'restaurant_id' => 3,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Burritos',
+        'description' => 'Piadine riempite con carne',
+        'path_img' => 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 5.99,
+        'visible' => true,
+        'restaurant_id' => 3,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Churros',
+        'description' => 'Pasta fritta',
+        'path_img' => 'https://images.pexels.com/photos/372851/pexels-photo-372851.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 7.99,
+        'visible' => true,
+        'restaurant_id' => 3,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Involtini',
+        'description' => 'Involtini di primavera',
+        'path_img' => 'https://www.giallozafferano.it/images/ricette/230/23004/foto_hd/hd650x433_wm.jpg',
+        'price' => 6.99,
+        'visible' => false,
+        'restaurant_id' => 4,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Nuvole',
+        'description' => 'Nuvole di drago',
+        'path_img' => 'https://www.buttalapasta.it/wp-content/uploads/2012/06/nuvolette-di-drago.jpg',
+        'price' => 3.99,
+        'visible' => true,
+        'restaurant_id' => 4,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Anatra',
+        'description' => 'Anatra alla pechinese',
+        'path_img' => 'https://www.gustissimo.it/articoli/ricette/pollame/anatra-alla-pechinese.jpg',
+        'price' => 9.99,
+        'visible' => true,
+        'restaurant_id' => 4,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Big Mac',
+        'description' => 'Hamburger di manzo',
+        'path_img' => 'https://www.junkfully.com/wp-content/uploads/2016/12/Big-Mac.png',
+        'price' => 5.80,
+        'visible' => true,
+        'restaurant_id' => 5,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Toast',
+        'description' => 'Toast di tacchino e formaggio',
+        'path_img' => 'https://www.mcdonalds.it/sites/default/files/styles/compressed/public/products/mctoast_mobile.jpg.webp?itok=V1RNXrh6',
+        'price' => 3.50,
+        'visible' => true,
+        'restaurant_id' => 5,
+        'slug' => ''
+    ],
+    [
+        'name' => 'Patatine',
+        'description' => 'Patatine fritte',
+        'path_img' => 'https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        'price' => 2.50,
+        'visible' => true,
+        'restaurant_id' => 5,
+        'slug' => ''
+    ] 
+];
---
+++ config/restaurants.php
@@ -0,0 +1,49 @@
+<?php 
+// Esempi di ristoranti
+return [
+    [
+        'restaurant_name' => 'Trattoria la Trebia',
+        'address' => 'Via Trebbia, 32',
+        'vat' => 47322067810,
+        'phone' => 0445734401,
+        'path_img' => 'https://media-cdn.tripadvisor.com/media/photo-s/13/80/8e/42/la-nostra-cantina.jpg',
+        'user_id' => 1,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Osteria Conchetta',
+        'address' => 'Via Conchetta, 8',
+        'vat' => 35112390561,
+        'phone' => 0445734561,
+        'path_img' => 'https://www.osteriaconchetta.it/assets/gallery-slides/gallery-2.jpg',
+        'user_id' => 2,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Il ristorantino',
+        'address' => 'Via degli Artiglieri, 7',
+        'vat' => 36112079401,
+        'phone' => 0445633216,
+        'path_img' => 'https://media-cdn.tripadvisor.com/media/photo-s/16/6b/b0/35/il-ristorantino-interni.jpg',
+        'user_id' => 3,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Sapori Stellati',
+        'address' => 'Via dei sapori, 53',
+        'vat' => 56448309193,
+        'phone' => 0445633213,
+        'path_img' => 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/17416590-a368-4dca-b52d-8a8dd0c98fdc/c6835411-70ee-45c6-b318-63d746c74f71.jpg',
+        'user_id' => 4,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Mc Donalds',
+        'address' => 'Via delle Arti, 95',
+        'vat' => 36650989012,
+        'phone' => 0445744510,
+        'path_img' => 'https://forbes.it/wp-content/uploads/2020/03/mcdonalds-fast-food-shutterstock.jpg',
+        'user_id' => 5,
+        'slug' => ''
+    ]
+];
