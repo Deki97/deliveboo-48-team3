89bbc83a0e08ace0240b58b942d2ddfdf68fa6ef
tgiannotta <t.giannotta92@gmail.com>
2022-04-01 12:06:49 +0200

    edit image in restaurant detail

--- resources/js/pages/RestaurantDetail.vue
+++ resources/js/pages/RestaurantDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-color">
+  <div>
     <HeaderRestaurant />
     <div class="container-fluid px-md-5">
       <div>
@@ -26,12 +26,11 @@
                 v-for="(dish, index) in dishes"
                 :key="dish.id + index"
               >
-                <div
-                  class="pro-pic"
-                  v-bind:style="{
-                    'background-image': 'url(' + imgURL + dish.path_img + ')',
-                  }"
-                ></div>
+                <div class="pro-pic"
+                 
+                >
+                  <!-- <img :src="dish.path_img" alt=""> -->
+                </div>
                 <div class="description-wrap mw-100 text-center">
                   <div class="description">
                     <h5 class="plate-name">{{ dish.name }}</h5>
@@ -141,27 +140,18 @@
         </div>
       </div>
     </div>
-  
-    <div>
-      <Footer />
-    </div>
-    
   </div>
-
 </template>
 <script>
 import ModalCart from "../components/ModalCart.vue";
 import Checkout from "../components/Checkout.vue";
 import HeaderRestaurant from "../components/HeaderRestaurant.vue";
-import Footer from "../components/Footer.vue";
-
 export default {
   name: "Cart",
   components: {
     ModalCart,
     Checkout,
-    HeaderRestaurant,
-    Footer
+    HeaderRestaurant
   },
   data() {
     return {
@@ -257,36 +247,11 @@ export default {
 @import "../../sass/app.scss";
 body {
   margin: 0 !important;
-
 }
-.bg-color{
-  background: linear-gradient(67deg, #7c2a02, #121c19, #f3ba32);
-       background-size: 600% 600%;
 
-        -webkit-animation: AnimationName 23s ease infinite;
-         -moz-animation: AnimationName 23s ease infinite;
-         animation: AnimationName 23s ease infinite;
-    
-
-     @-webkit-keyframes AnimationName {
-         0%{background-position:51% 0%}
-         50%{background-position:50% 100%}
-         100%{background-position:51% 0%}
-     }
-     @-moz-keyframes AnimationName {
-         0%{background-position:51% 0%}
-         50%{background-position:50% 100%}
-         100%{background-position:51% 0%}
-     }
-     @keyframes AnimationName {
-         0%{background-position:51% 0%}
-         50%{background-position:50% 100%}
-         100%{background-position:51% 0%}
-     }
-}
 .card {
   margin-top: 100px;
-  /* height: 300px; */
+  height: 500px;
   position: relative;
   background-color: #fec866;
   transition: all 0.3s ease-out;
@@ -314,7 +279,6 @@ body {
   opacity: 0;
   transform: translateY(0%);
   opacity: 1;
-  padding-top: 60px;
 }
 .card .description > * {
   width: 100%;
