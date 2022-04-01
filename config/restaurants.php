fa364e37921c2af9957f0d61ccd73efcadf59a7a
hexeva <patrick79.g@gmail.com>
2022-04-01 17:31:52 +0200

    modified database config files

--- config/restaurants.php
+++ config/restaurants.php
@@ -20,11 +20,11 @@ return [
         'slug' => ''
     ],
     [
-        'restaurant_name' => 'Il ristorantino',
+        'restaurant_name' => 'Ristorante Weng Long',
         'address' => 'Via degli Artiglieri, 7',
         'vat' => 36112079401,
         'phone' => 0445633216,
-        'path_img' => 'https://media-cdn.tripadvisor.com/media/photo-s/16/6b/b0/35/il-ristorantino-interni.jpg',
+        'path_img' => 'https://hokkaidoguide.com/wp-content/uploads/2020/07/126A0393-scaled.jpg',
         'user_id' => 3,
         'slug' => ''
     ],
@@ -38,12 +38,57 @@ return [
         'slug' => ''
     ],
     [
-        'restaurant_name' => 'Mc Donalds',
+        'restaurant_name' => 'Mc Davids',
         'address' => 'Via delle Arti, 95',
         'vat' => 36650989012,
         'phone' => 0445744510,
         'path_img' => 'https://forbes.it/wp-content/uploads/2020/03/mcdonalds-fast-food-shutterstock.jpg',
         'user_id' => 5,
         'slug' => ''
-    ]
+    ],
+    [
+        'restaurant_name' => 'Tihuana',
+        'address' => 'Via dei Vizi, 30',
+        'vat' => 90877878672,
+        'phone' => 0445764510,
+        'path_img' => 'https://image.shutterstock.com/image-photo/glowing-interior-loft-style-mexican-600w-430097332.jpg',
+        'user_id' => 6,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Yukine',
+        'address' => 'Viale dei Mille, 8',
+        'vat' => 97777878672,
+        'phone' => 05457664510,
+        'path_img' => 'https://image.shutterstock.com/image-photo/osaka-japan-december-27-locals-600w-248540572.jpg',
+        'user_id' => 7,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Burger time',
+        'address' => 'Vicolo dei loschi, 18',
+        'vat' => 33477878552,
+        'phone' => 0553464650,
+        'path_img' => 'https://thumbs.dreamstime.com/b/subway-fast-food-restaurant-interior-nice-france-august-american-franchise-primarily-sells-65892244.jpg',
+        'user_id' => 8,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Anemos',
+        'address' => 'Piazza della Liberazione, 26',
+        'vat' => 54479888552,
+        'phone' => 0663464770,
+        'path_img' => 'https://media.thatsweetgift.com/wp-content/uploads/2019/06/best-greek-restaurants.jpg',
+        'user_id' => 9,
+        'slug' => ''
+    ],
+    [
+        'restaurant_name' => 'Rincao de Bahia',
+        'address' => 'Viale Vittorio Veneto, 8',
+        'vat' => 74559758552,
+        'phone' => 0553464120,
+        'path_img' => 'https://www.conunviaggionellatesta.it/wordpress/wp-content/uploads/2015/05/lapa-nuove-low-999.jpg',
+        'user_id' => 10,
+        'slug' => ''
+    ],
 ];
